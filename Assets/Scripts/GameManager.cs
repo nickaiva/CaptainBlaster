37f4ee3b993ae70dc698f0c12c5016b55c2196ce
Nick Aiva <nickaiva@yahoo.gr>
2023-04-17 11:53:10 +0300

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text gameOverText;
+	int playerScore = 0;
+	public void AddScore()
+	{
+		playerScore++;
+		// This converts the score (a number) into a string
+		scoreText.text = playerScore.ToString();
+	}
+	public void PlayerDied()
+	{
+		gameOverText.enabled = true;
+		// This freezes the game
+		Time.timeScale = 0;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
